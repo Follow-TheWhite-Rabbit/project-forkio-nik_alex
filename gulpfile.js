fcb699c18dab0db28b76b2dc634662bb5aac2813
Никита Голуб <sleipnir1148@gmail.com>
2022-11-03 22:30:43 +0200

    feat: create folder src, and files: gulpfile.js and package.json

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+import gulp from'gulp';
+import concat from'gulp-concat';
+import imagemin from'gulp-imagemin';
+import clean from'gulp-clean';
+import BS from 'browser-sync';
+const  browserSync = BS.create();
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const  sass = gulpSass(dartSass);
+
+const concatJsFunc = () => gulp.src('src/js/**/*')
+    .pipe(concat("all.js"))
+    .pipe(gulp.dest('dist/js'));
+
+
+const convertCss = () => gulp.src('src/scss/**/*', { nodir: true })
+    .pipe(sass())
+    .pipe(concat('styles.min.css'))
+    .pipe(gulp.dest('dist/css'));
+
+
+const minifyImages = () => gulp.src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+
+
+const cleanDist = () => gulp.src('dist/*', {read: false}).pipe(clean())
+
+
+gulp.task('concatJs', concatJsFunc);
+gulp.task('convertCss', convertCss);
+gulp.task('minifyImages', minifyImages);
+gulp.task('cleanDist', cleanDist);
+gulp.task('build', gulp.series('cleanDist','concatJs','convertCss','minifyImages'));
+gulp.task('buildCreatingProject', gulp.series(cleanDist, convertCss, concatJsFunc))
+
+
+
+const startWatching = () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('src/**/*').on('change', gulp.series(convertCss,concatJsFunc,minifyImages, browserSync.reload))
+
+}
+
+gulp.task('dev', startWatching);
+
+
