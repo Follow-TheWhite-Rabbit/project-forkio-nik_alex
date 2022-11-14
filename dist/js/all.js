8b3cd5fdb982f871ee7ea97e813eedb806cbebcd
Sayonara <alekss3228@gmail.com>
2022-11-14 22:25:00 +0200

    features_section-fix2.0

--- dist/js/all.js
+++
@@ -1,6 +0,0 @@
-const burgerButton = document.querySelector('.hero__burger-button');
-const burgerMenu = document.querySelector('.hero__content-burgerMenu');
-burgerButton.addEventListener('click', e => {
-    burgerMenu.classList.toggle('hero__content-burgerMenu--active')
-    burgerButton.classList.toggle('active-burger')
-})
